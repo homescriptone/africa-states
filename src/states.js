18d41b611c4b8458f5c1fc9e5483ebefb26db670
Manu The Blacker <25031292+manutheblacker@users.noreply.github.com>
2021-10-29 13:39:02 +0100

    Add list of states file

---
+++ src/states.js
@@ -0,0 +1,33 @@
+exports.states_lists = {
+    'BJ' : { // Benin
+        'AT' : 'Atakora',
+        'AL' : 'Alibori',
+        'AT' : 'Atlantique',
+        'BG' : 'Borgou',
+        'CO' : 'Collines',
+        'DO' : 'Donga',
+        'KO' : 'Couffo',
+        'LI' : 'Littoral',
+        'MO' : 'Mono',
+        'OU' : 'Ouémé',
+        'PL' : 'Plateau',
+        'ZO' : 'Zou'
+    },
+
+    'CIV' : { // Côte d'Ivoire
+        'AB' : 'Abidjan',
+        'BS' : 'Bas-Sassandra',
+        'CM' : 'Comoé',
+        'DN' : 'Denguélé',
+        'GD' : 'Gôh-Djiboua',
+        'LC' : 'Lacs',
+        'LG' : 'Lagunes',
+        'MG' : 'Montagnes',
+        'SM' : 'Sassandra-Marahoué',
+        'SV' : 'Savanes',
+        'VB' : 'Vallée du Bandama',
+        'WR' : 'Woroba',
+        'YM' : 'Yamoussoukro',
+        'ZZ' : 'Zanzan'
+    }
+}
